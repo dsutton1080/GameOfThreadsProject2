46234dc00cbfc028c30c2c4d681a7a61df555d64
jace-kline <jace_kline@ku.edu>
2019-10-17 18:34:12 -0500

    Added guess method defs to the AI class. Each method returns a Coord struct with the row and col for where the AI is guessing

--- AI.h
+++ AI.h
@@ -2,6 +2,8 @@
 #define AI_H
 
 #include "Players.h"
+#include "Utils.cpp"
+
 class AI : public Players {
 private:
 	int difficulty;
@@ -10,6 +12,28 @@ public:
 	AI(int difficulty);
 	void setShips(int);
 	bool isAI();
+
+	/* 
+	- Invoked when its the AI's turn
+	- Returns a 'Coord' struct containing the "row" and "col" properties (both of type int)
+	- The returned Coord object should have both row, col in the range 0-7
+	- The hard guess will need to have access to the other player's data to know where the
+	ships are. This is why it has a pointer to the other player
+	- Might be useful to create a generalized 'randomCoord()' function in Utils to provide
+	a random Coord object, useful in any of the other methods using the combination of a
+	randomly-generated row and column.
+	- See 'Utils.cpp' file for the Coord struct definition
+	*/
+
+	// Random every guess. Ensure that you don't guess what's already been guessed.
+	Coord easyGuess();
+
+
+	Coord mediumGuess();
+
+	// Must use the pointer to the other player to know where the other player's ships are.
+	// Be sure to keep straight which spots on the other player's have already been guessed 
+	Coord hardGuess(Players* otherPlayer);
 };
 #endif
 
