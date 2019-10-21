832d986394558bf0b0a8ae903e621aaaf5e3b315
DeRon Sutton <dsutton1080@gmail.com>
2019-10-20 22:54:56 -0500

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -17,81 +17,81 @@ struct Coord {
     bool operator==(Coord c) const;
 };
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordinate object, and the bounds for the grid 
+*@pre Needs to be passed a non empty Coord object and ints that are not empty 
+*@post Returns true if coord is in bounds and false otherwise 
 */
 bool isCoordInBounds(Coord c, int rowLow, int rowHigh, int colLow, int colHigh);
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordinate object 
+*@pre coordinate object must not be empty 
+*@post returns true if the coordinate is in bounds and false otherwise 
 */
 bool isValidCoord(Coord c);
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordiante object that is an Array 
+*@pre Coord object must not be empty 
+*@post returns true if all Coords are in bounds and false otherwise 
 */
 bool isValidCoords(Coord coordArr[]);
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordinate object and number representing the specialShotType
+*@pre Coord object must not be empty and int must not be null
+*@post returns true is coordinate is locationally valid 
 */
 bool isValidCenterCoord(Coord c, int specialShotType);
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordinate object 
+*@pre helper function 
+*@post returns a reference coordinate object 
 */
 Coord* coordsFromRowColOffsets(Coord c, Coord* offsets);
 /**
-*@param
-*@pre
-*@post
+*@param takes a shotType as an integer and the origin coordinate for shot type
+*@pre shotType must not be null and Coord must not be empty 
+*@post returns a reference coordinate object 
 */
 Coord* specialShotToCoords(int shotType, Coord center);
 /**
-*@param
-*@pre
+*@param takes a coords list and the coordinates 
+*@pre Coords must not be empty and the row and col must not be null 
 *@post
 */
 bool isInCoords(Coord coords[], int row, int col);
 /**
-*@param
-*@pre
-*@post
+*@param takes a coordinate object 
+*@pre helper function 
+*@post returns true if the coord passed in is a valid special shot coordinate
 */
 bool isValidCenterSpecialShotCoord(Coord c, int shotEncoding);
 /**
-*@param
-*@pre
-*@post
+*@param takes a string as a message and the max and min acceptable cin response
+*@pre the min and max must not be null 
+*@post returns an integer 
 */
 int getRowInput(std::string msg, int min, int max);
 /**
-*@param
-*@pre
-*@post
+*@param takes a string as a message and the max and min acceptable cin response
+*@pre the min and max must not be null 
+*@post returns an integer 
 */
 char getColumnInput(std::string msg, int min, int max);
 /**
-*@param
-*@pre
-*@post
+*@param takes a character 
+*@pre character must not be empty 
+*@post returns a integer that was a conversion of that character 
 */
 int charConvert(char temp);
 /**
-*@param
-*@pre
-*@post
+*@param takes an integer 
+*@pre integer must not be null
+*@post returns a character that was a conversion of that integer
 */
 char charConvertInverse(int n);
 /**
-*@param
-*@pre
-*@post
+*@param takes an integer
+*@pre integer must not be null
+*@post returns a string that is the name of that respective integer value 
 */
 std::string toShipName(int i);
 
